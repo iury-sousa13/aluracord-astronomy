d1821781c1a8f6ba8c031eeb65251eea1f750ca9
IuryWemerson <iury_wemerson@hotmail.com>
2022-06-05 02:17:46 -0300

    add analytic

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
             rel="stylesheet"
           />
+          <script
+            defer
+            data-domain="aluracord-astronomy.vercel.app"
+            src="https://plausible.io/js/plausible.js"
+          />
         </Head>
         <body>
           <Main />
